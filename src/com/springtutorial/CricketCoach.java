80a276a47a09bd03188583f486655dd473898da3
RP57547 <RP57547@AWSW56SLPC0182.ind.zensar.com>
2020-03-28 19:02:41 +0530

    Construtor Injection Example!

---
+++ src/com/springtutorial/CricketCoach.java
@@ -0,0 +1,17 @@
+package com.springtutorial;
+
+public class CricketCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Daily do bowling practice for 2 hrs.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
